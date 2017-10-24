dd15bccdb97a960468ee3b81388d5ed7e8264a6b
Henrique Lavôr <henriquelavor@gmail.com>
2017-10-24 19:12:31 -0400

    Projeto criado

---
+++ src/main/java/com/henriquelavor/sigpedidos/SigpedidosApplication.java
@@ -0,0 +1,12 @@
+package com.henriquelavor.sigpedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigpedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigpedidosApplication.class, args);
+	}
+}
---
+++ src/test/java/com/henriquelavor/sigpedidos/SigpedidosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.henriquelavor.sigpedidos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SigpedidosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
