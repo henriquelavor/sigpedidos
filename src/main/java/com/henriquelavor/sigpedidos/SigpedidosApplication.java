0fdd6e8f3fcb16f207e8a57155cd4536e887c778
Henrique Lavôr <henriquelavor@gmail.com>
2017-12-28 22:41:34 -0400

    Paginando com parametros opcionais na requisicao

--- src/main/java/com/henriquelavor/sigpedidos/SigpedidosApplication.java
+++ src/main/java/com/henriquelavor/sigpedidos/SigpedidosApplication.java
@@ -74,6 +74,11 @@ public class SigpedidosApplication implements CommandLineRunner{
 		
 		Categoria cat1 =  new Categoria(null,"Informática");
 		Categoria cat2 =  new Categoria(null,"Escritório");
+		Categoria cat3 =  new Categoria(null,"Cama mesa e banho");
+		Categoria cat4 =  new Categoria(null,"Eletrônicos");
+		Categoria cat5 =  new Categoria(null,"Jardinagem");
+		Categoria cat6 =  new Categoria(null,"Decoração");
+		Categoria cat7 =  new Categoria(null,"Perfumaria");
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -99,7 +104,7 @@ public class SigpedidosApplication implements CommandLineRunner{
 		est2.getCidades().addAll(Arrays.asList(c2,c3));
 		
 		
-		categoriaRepository.save(Arrays.asList(cat1,cat2));
+		categoriaRepository.save(Arrays.asList(cat1,cat2, cat3, cat4, cat5, cat6, cat7));
 		produtoRepository.save(Arrays.asList(p1, p2, p3));
 
 		estadoRepository.save(Arrays.asList(est1,est2));
