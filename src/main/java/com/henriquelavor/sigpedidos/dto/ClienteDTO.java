f955212e47337f752bba6909a7ee2210b8766a1a
Henrique Lavôr <henriquelavor@gmail.com>
2017-12-29 19:45:39 -0400

    Validacao personalizada: email nao repetido na atualizacao de cliente

--- src/main/java/com/henriquelavor/sigpedidos/dto/ClienteDTO.java
+++ src/main/java/com/henriquelavor/sigpedidos/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.henriquelavor.sigpedidos.domain.Cliente;
+import com.henriquelavor.sigpedidos.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
