d144cc889199a25cc4bc155057cc84341a5622eb
Henrique Lavôr <henriquelavor@gmail.com>
2018-01-02 17:45:56 -0400

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/henriquelavor/sigpedidos/services/DBService.java
+++ src/main/java/com/henriquelavor/sigpedidos/services/DBService.java
@@ -124,7 +124,7 @@ public class DBService {
 		cidadeRepository.save(Arrays.asList(c1,c2,c3));
 		
 		
-		Cliente cli1 = new Cliente(null, "Maria Gabriela","maria@gmail.com","36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Gabriela","henrique.lavor@yahoo.com","36378912377", TipoCliente.PESSOAFISICA);
 		cli1.getTelefones().addAll(Arrays.asList("9536231178","95991611012"));
 		
 		Endereco e1 = new Endereco(null, "Rua Flores", "475", "Casa própria", "Centenário", "69305317", cli1, c1);
