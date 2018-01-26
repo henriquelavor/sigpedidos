5ddb4ee2df40e344861df6739567b48ba275aaa5
Henrique Lavôr <henriquelavor@gmail.com>
2018-01-25 23:49:09 -0400

    spring.profiles.active=dev

--- src/main/java/com/henriquelavor/sigpedidos/SigpedidosApplication.java
+++ src/main/java/com/henriquelavor/sigpedidos/SigpedidosApplication.java
@@ -1,5 +1,6 @@
 package com.henriquelavor.sigpedidos;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -7,10 +8,16 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.henriquelavor.sigpedidos.services.S3Service;
+
+
 
 @SpringBootApplication
 @RestController
 public class SigpedidosApplication extends SpringBootServletInitializer{
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SigpedidosApplication.class, args);
@@ -23,10 +30,7 @@ public class SigpedidosApplication extends SpringBootServletInitializer{
 	
 	@RequestMapping(value="/")
 	public String demo() {
+		s3Service.uploadFile("/Users/henriquelavor/Downloads/henrique.jpg");
 		return "Aplicação iniciada!";
 	}
-	
-	
-	
-	
 }
--- src/main/java/com/henriquelavor/sigpedidos/SigpedidosApplicationbackup.java
+++ src/main/java/com/henriquelavor/sigpedidos/SigpedidosApplicationbackup.java
@@ -1,4 +1,9 @@
 /*package com.henriquelavor.sigpedidos;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.henriquelavor.sigpedidos.services.S3Service;
+
+
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -7,6 +12,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SigpedidosApplicationbackup implements CommandLineRunner{
+
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SigpedidosApplicationbackup.class, args);
@@ -14,7 +22,7 @@ public class SigpedidosApplicationbackup implements CommandLineRunner{
 	
 	@Override
 	public void run(String... arg0) throws Exception {
-		
+		s3Service.uploadFile("/Users/henriquelavor/Downloads/henrique.jpg");
 		 
 	}
 	
